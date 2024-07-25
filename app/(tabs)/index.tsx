b8981a7a6b3b52e51726dcdf340e9bdfe386e9cb
eria-developer <eriddeveloper@gmail.com>
2024-07-25 13:42:23 +0300

    last commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
 const home = () => {
   return (
-    <View>
-      <Text>home</Text>
+    <View style={{ marginHorizontal: 15 }}>
+      <View>
+        <Text>
+          In this simple app, we explore how to add a file tab within the tabs
+          directory but you dont want it to be displayed among the tabs
+        </Text>
+        <Text>
+          Forexample we have added the just page among tabs, but it is not being
+          displayed.
+        </Text>
+        <Text>
+          We just have to added a href option and set it to null, in the layout
+        </Text>
+      </View>
+      <View style={{ paddingTop: 20 }}>
+        <Link href="just" style={{ color: "blue", fontWeight: "600" }}>
+          Go to Just tab
+        </Link>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default home
+export default home;
