827bb267ba427fd8977f49dd1e61dd3113139500
eria-developer <eriddeveloper@gmail.com>
2024-07-25 13:25:54 +0300

    layout mande

--- README.md
+++
@@ -1,2 +0,0 @@
-This simple react native expo project displays one thing.
-I you have a file among the (tabs) folder but you dont want it to be displayed among the tab bar
---
+++ app/(tabs)/just.tsx
@@ -0,0 +1,14 @@
+// This is the file which i dont want to be displayed among the tab bars
+
+import { View, Text } from "react-native";
+import React from "react";
+
+const just = () => {
+  return (
+    <View>
+      <Text>just</Text>
+    </View>
+  );
+};
+
+export default just;
